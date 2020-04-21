ea5b5d5230d647973e9d4684154ea8127e8680f6
Andrey Vyalkov <dushkakruf@mail.ru>
2020-04-21 10:40:43 +0500

    favicon

--- season/season/settings.py
+++ season/season/settings.py
@@ -1,19 +1,12 @@
-"""
-Django settings for season project.
-"""
+# Django settings for season project.
 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'aw@&9ld7%xwj#!&_znstfywptq#^$l2t9tksvrkdam*mi$u%__'
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'aw@&9ld7%xwj#!&_znstfywptq#^$l2t9tksvrkdam*mi$u%__')
 
 ADMINS = (
     ('Andrey Vyalkov', 'dushkakruf@mail.ru'),
@@ -21,7 +14,6 @@ ADMINS = (
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['sezon66.ru', '127.0.0.1']
 
@@ -41,7 +33,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,10 +61,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'season.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -81,10 +69,7 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -100,9 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
 
@@ -116,7 +99,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
